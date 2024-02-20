53cd93251942820860c693aeaac5ba55c68bbeaf
Wayne Denise Dizon <wddizon@student.apc.edu.ph>
2024-02-20 08:37:00 +0800

    Added CI4 News Tutorial Code

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -9,8 +9,11 @@ $routes->get('/', 'Home::index');
 
 
 // app/Config/Routes.php
-
+use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 
 $routes->get('/', 'Home::index');
 $routes->get('pages', [Pages::class, 'index']);
