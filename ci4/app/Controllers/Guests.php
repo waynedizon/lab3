abfe0582211364ce6567ef73f9cba69e01b07ef4
Wayne Denise Dizon <wddizon@student.apc.edu.ph>
2024-03-07 22:19:29 +0800

    edited guests.php

--- ci4/app/Controllers/Guests.php
+++ ci4/app/Controllers/Guests.php
@@ -1,87 +1,91 @@
 <?php
-
+ 
 namespace App\Controllers;
-
-use App\Models\GuestsModel;
+ 
+use App\Models\GuestModel;
 use CodeIgniter\Exceptions\PageNotFoundException;
-
+ 
 class Guests extends BaseController
 {
     public function index()
     {
-        $model = model(GuestsModel::class);
-
+        $model = model(GuestModel::class);
+ 
+       
         $data = [
-            'guest'  => $model->getGuest(),
+            'guests'  => $model->getGuests(),
             'title' => 'Guest List',
         ];
-
-        return view('templates/header', $data)
+ 
+        return view('templates/guestheader', $data)
             . view('guests/index')
-            . view('templates/footer');
+            . view('templates/guestfooter');        
+ 
+ 
     }
-
+ 
     public function show($email = null)
     {
-        $model = model(GuestsModel::class);
-
+        $model = model(GuestModel::class);
+ 
         $data['guests'] = $model->getGuests($email);
-
+ 
         if (empty($data['guests'])) {
             throw new PageNotFoundException('Cannot find the guest item: ' . $email);
         }
-
+ 
         $data['title'] = $data['guests']['name'];
-
-        return view('templates/header', $data)
+ 
+        return view('templates/guestheader', $data)
             . view('guests/view')
-            . view('templates/footer');
+            . view('templates/guestfooter');
     }
-
-    public function new()
+ 
+public function new()
     {
         helper('form');
-
-        return view('templates/header', ['title' => 'Create a guest item'])
+ 
+        return view('templates/guestheader', ['title' => 'Create a guest item'])
             . view('guests/create')
-            . view('templates/footer');
+            . view('templates/guestfooter');
     }
-
+ 
     public function create()
     {
         helper('form');
-
-        $data = $this->request->getPost(['name', 'email', 'website', 'comment', 'gender']);
-
+ 
+        $data = $this->request->getPost(['name', 'email', 'comment']);
+ 
         // Checks whether the submitted data passed the validation rules.
         if (! $this->validateData($data, [
             'name' => 'required|max_length[255]|min_length[3]',
-            'email' => 'required|max_length[255]|min_length[3]',
-            'website' => 'required|max_length[255]|min_length[3]',
-            'comment'  => 'required|max_length[5000]|min_length[10]',
-            'gender' => 'required|max_length[255]|min_length[3]',
+            'email'  => 'required|max_length[255]|min_length[5]',
+            'comment'  => 'required|max_length[5500]|min_length[10]',
+ 
         ])) {
             // The validation fails, so returns the form.
             return $this->new();
         }
-
+ 
         // Gets the validated data.
         $post = $this->validator->getValidated();
-
-        $model = model(GuestsModel::class);
-
+ 
+        $model = model(GuestModel::class);
+ 
         $model->save([
             'name' => $post['name'],
-            'email' => $post['email'],
-            'website' => $post['website'],
-            'comment' => $post['comment'],
-            'gender' => $post['gender'],
+            'email'  => $post['email'],
+            'comment'  => $post['comment'],
+ 
+ 
         ]);
-
-	header("refresh: 3");
-
-        return view('templates/header', ['title' => 'Create a guest item'])
+ 
+        return view('templates/guestheader', ['title' => 'Create a guest item'])
             . view('guests/success')
-            . view('templates/footer');
+            . view('templates/guestfooter');
     }
+ 
+ 
+ 
+ 
 }
