7e94ad80f8bffcf4d17daf16bdd9e1b32ca41228
Wayne Denise Dizon <wddizon@student.apc.edu.ph>
2024-03-07 21:12:22 +0800

    guest sign up bonus task - updated routes.php

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -13,7 +13,9 @@ use App\Controllers\Guests;
 use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
 
-$routes->get('guests', [Guests::class, 'index']);           // Add this line
+$routes->get('guests', [Guests::class, 'index']);
+$routes->get('guests/new', [Guests::class, 'new']);
+$routes->post('guests', [Guests::class, 'create']);           // Add this line
 $routes->get('guests/(:segment)', [Guests::class, 'show']);
 
 $routes->get('news', [News::class, 'index']); 
